59ab491bc846bcd250d09009a2fb2505408ce93a
Ricardosgeral <ricardos.geral@gmail.com>
2024-07-04 19:34:52 +0100

    revert cookies deletion

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,18 +1,8 @@
 "use server";
 
 import { signOut } from "@/auth";
-import { cookies } from "next/headers";
 
 export const logout = async () => {
-  const allCookies = cookies().getAll();
-  // Iterate through all cookies and delete then
-  allCookies.forEach((cookie) => {
-    cookies().set({
-      name: cookie.name,
-      value: "",
-      path: "/",
-      maxAge: -1, // This will cause the cookie to be deleted
-    });
-  });
+  //some server stuff if needed
   await signOut({ redirectTo: "/", redirect: true });
 };
